b77e37580df69ad6411c8c0c5efb1aa2876a8a26
Olivier Mouchet <mouchet@pv.com>
2014-02-03 14:16:15 +0100

    untar .vim archive in install script

--- apply_setup.sh
+++ apply_setup.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 DF_DIR=${PWD}/dotfiles
 
 [ ! -d ${DF_DIR} ] && return 1;
+
+[ -e ${DF_DIR}/.vim.tgz ] && [ ! -d ${DF_DIR}/.vim ] && pushd ${DF_DIR} && /bin/tar -xzf .vim.tgz && popd
 
 for df in \
 	.gitconfig \
@@ -15,3 +17,4 @@ for df in \
 	ln -s ${DF_DIR}/${df} ${HOME}/${df}
 	[ $? -eq 0 ] && echo "setup successfully ${df}"
 done
+
