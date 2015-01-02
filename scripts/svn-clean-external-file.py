1d4e1915490ae97e012f9ec98d0111dc35a6a4c9
Olivier Mouchet <mouchet@pv.com>
2015-01-02 11:03:36 +0100

    improve svn-clean-external-file.py

--- scripts/svn-clean-external-file.py
+++ scripts/svn-clean-external-file.py
@@ -5,17 +5,23 @@ import os
 import re
 
 def removeall(path):
-    if not os.path.isdir(path):
-        os.remove(path)
-        return
-    files=os.listdir(path)
-    for x in files:
-        fullpath=os.path.join(path, x)
-        if os.path.isfile(fullpath):
-            os.remove(fullpath)
-        elif os.path.isdir(fullpath):
-            removeall(fullpath)
-    os.rmdir(path)
+    try:
+        if not os.path.isdir(path):
+            os.remove(path)
+	    print("remove %s"%path)
+            return
+        files=os.listdir(path)
+        for x in files:
+            fullpath=os.path.join(path, x)
+            if os.path.isfile(fullpath):
+                os.remove(fullpath)
+		print("removefull %s"%fullpath)
+            elif os.path.isdir(fullpath):
+                removeall(fullpath)
+        os.rmdir(path)
+	print("rmdir %s"%path)
+    except:
+        pass
 
 unversionedRex = re.compile('^ ?[\?ID] *[1-9 ]*[a-zA-Z]* +(.*)')
 for l in  os.popen('svn status --no-ignore -v').readlines():
